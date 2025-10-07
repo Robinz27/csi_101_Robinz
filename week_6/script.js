271804c425e3f05330557b55aa6bf2dbba792edd
BANGKHEN\5-903 <ictservice@1.spu.ac.th>
2025-10-07 10:39:26 +0700

    update

--- week_6/script.js
+++ week_6/script.js
@@ -1,21 +1,33 @@
 document.writeln("<h1>Hello Javascript</h1>")
 console.log("Where is Ailey going")
 // alert("username or password failed")
-let a = 15
-let b = 15
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
+let a1 = 15
+let b2 = 15
+console.log(a1+b2)
+console.log(a1-b2)
+console.log(a1*b2)
+console.log(a1/b2)
 let age
 let fname = "Lisa"
 let flag = true
 age = 19
 document.writeln("age = ", age)
-age = 16
+age = "16"
 console.log(age)
 document.writeln("<br>First Name = ", fname)
 console.log(typeof(age))
 console.log(typeof(sum))
 console.log(typeof(fname))
 console.log(typeof(flag))
+
+
+let a,b,c
+a = 5
+b = 3
+c = "2"
+total = a + b
+console.log("total = ",total)
+console.log(typeof(total))
+total2 = a + c
+console.log("total2 = ",total2)
+console.log(typeof(total2))
