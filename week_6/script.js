91fe1c07743c205053227b2574ada9296074aa10
BANGKHEN\5-903 <ictservice@1.spu.ac.th>
2025-10-07 10:06:39 +0700

    let , console.log

--- week_6/script.js
+++ week_6/script.js
@@ -1,1 +1,9 @@
-document.write("Javascript")
+document.writeln("<h1>Hello Javascript</h1>")
+console.log("Where is Ailey going")
+// alert("username or password failed")
+let a = 15
+let b = 15
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
