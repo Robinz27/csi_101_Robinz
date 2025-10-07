5d8320ac8c86223d7e36c989db88a20bb9064895
BANGKHEN\5-903 <ictservice@1.spu.ac.th>
2025-10-07 11:40:04 +0700

    fuc js

--- week_6/java.js
+++ week_6/java.js
@@ -2,5 +2,13 @@ const h1 = document.getElementById("h01")
 console.log(h1)
 const img1 = document.getElementById("img01")
 console.log(img1)
-h1.innerHTML = "Hello Aily"
-img1.src = "img/chick.png"
+// h1.innerHTML = "Hello Aily"
+// img1.src = "img/chick.png"
+
+function main(){
+    console.log(main)
+    h1.innerHTML = "Hello Aily"
+    document.getElementById("img01").src="img/chick.png"
+    h1.style.color = "skyblue"
+    h1.style.textAlign = "center"
+}
